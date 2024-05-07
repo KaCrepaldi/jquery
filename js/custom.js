952565dc1e4a6f69d9da84bcd515ff59fbd877aa
Karla Crepaldi <163344451+KaCrepaldi@users.noreply.github.com>
2024-05-07 09:59:09 -0300

    atualizar alteraçoes

--- js/custom.js
+++ js/custom.js
@@ -19,4 +19,60 @@ $(document).ready(function(){
     $('.featured-item:first h4').append('<span class="bagde bg-secondary">Novo</span>')
 
 })
+// $('.featured-item:first h4').start('<span class="badge bg-secondary">Novo</span>')
+    // $('.featured-item:first h4').html('<span class="badge bg-secondary">Novo</span>')
+    // $('.featured-item:first h4').addClass('active')
+    // $('.featured-item:first h4').removeClass('active')
+    // $('.featured-item:first h4').toggleClass('active')
+    // $('.featured-item:first h4').hide()
+    // $('.featured-item:first h4').show()
+    // $('.featured-item:first h4').fadeIn(2000)
+    // $('.featured-item:first h4').fadeOut()
+    //  $('.featured-item:first h4').css('color', '#f00')
+     
+    $('.featured-item h4').dblclick( function(){
 
+        $(this).css({
+            'color': '#f00',
+            'background': '#ff0',
+            'font-weight': '100',
+        });
+
+     });
+
+     /*
+      * Manipulação de eventos
+      */
+     $('.featured-item a').on('blur', function(event){
+
+        event.preventDefault();
+
+        alert('Produto esgotado');
+
+     })
+
+
+$('.owl-carousel').owlCarousel({
+   loop: false,
+   margin: 10,
+   items: 1,
+   autoHeight: true,
+   autoWidth: true,
+   navText: [
+     "<i class='fa fa-caret-left'></i>",
+     "<i class='fa fa-caret-right'></i>"
+   ],
+   autoplay: true,
+   autoplayHoverPause: true,
+   responsive: {
+     0: {
+       items: 1
+     },
+     600: {
+       items: 3
+     },
+     1000: {
+       items: 3
+     }
+   }
+ })
